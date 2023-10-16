4bd7d40a59ecbd7f7c24b008db32aac362e7667f
Morgan Stenberg <morganstenberg@gmail.com>
2023-10-16 14:35:04 +0000

    Add introduction function

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+def introduction():
+    """
+    Introduces the game to the user and asks for username
+    """
+
+    print("Welcome to Whodunit Manor!\n")
+    print("You find yourself standing in front of the grand gates of Whodunit Manor, a sprawling estate shrouded in mystery.\n")
+    print("As Detective Extraordinaire, you've been called upon to solve the most perplexing case of the century - a murder most foul!\n")
+    print("Inside, you'll encounter a cast of eccentric characters, each with their own secrets and motives. The atmosphere is thick with tension, but fear not!\n")
+    print("Your wit, charm, and keen detective skills will be your greatest allies. Now, it's time to take your first step into the labyrinth of clues and eccentricities that is Whodunit Manor.\n")
+    print("Choose your path wisely, for the fate of this investigation rests squarely on your shoulders.\n")
+
+def main():
+    introduction()
+
+
+main()
