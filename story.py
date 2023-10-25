af87354ab9b6d29d4bb6422b1cf69d347dce326c
Morgan Stenberg <morganstenberg@gmail.com>
2023-10-25 08:10:57 +0000

    Add more branches of the story

--- story.py
+++ story.py
@@ -233,7 +233,43 @@ and the identity of Mortimer's killer remains a mystery.
 
 
 def branch_a1_1_2():
-    print("testing branch_a1_1_2")
+    print(
+    """
+You swiftly decide to follow the departing family members. Each takes a different 
+direction, leaving you with a moment of decision. The person closest to you, 
+their silhouette disappearing into the dimly lit chamber, catches your attention. 
+There's something about the way they glance back, a hint of suspicion in their eyes, 
+that makes you believe they may be holding onto some crucial information.
+
+Without further hesitation, you choose to tail them. They walk briskly down the corridor, 
+their steps purposeful. The echo of their heels against the polished floor seems to 
+reverberate through the silent mansion.
+
+Just as you round a corner, you catch sight of them disappearing into the room. 
+You quicken your pace, determined not to lose them.
+
+As you step into the room, the scene unfolds before you in an alarming tableau. 
+They stand with their back to you, their figure silhouetted against a large ornate mirror. 
+In their hand, you catch a glint of steel—a gun.
+
+Before you can react, they turn to face you, their expression a chilling mix of 
+determination and desperation. Without a word, they raise the gun..
+
+The world seems to slow for a heartbeat, and then everything happens at once. 
+The room fills with the echo of a gunshot, your instincts kicking into overdrive.
+
+But it's too late. In a single, fatal shot, they hit their mark.
+
+The room falls silent once more, the only sound the soft patter of rain against the window. 
+The truth of Mortimer's demise, along with the answers you sought, remain locked within the walls of Whodunit Manor.
+
+---
+
+The detective's pursuit of the truth takes an unforeseen turn, culminating in a deadly encounter. 
+The enigma of Whodunit Manor remains unsolved.
+
+    """)
+
 
 def branch_a1_2():
     print(
@@ -290,16 +326,17 @@ Lady Genevieve gasps, clutching her chest.
 Before anyone can react, the lights flicker and then go out entirely. 
 The room is plunged into darkness, and a collective gasp sweeps through the family.
 
-From the shadows, a voice breaks the silence. "I know what happened that night," 
-it declares, sending a shiver down your spine.
+In the chaos that follows, as everybody is trying to get the lights back on
+from the shadows, a voice whispers in your ear. 
 
-The room falls silent once more.. 
+"I know what happened that night, meet me in the basement.."
+
+As you try to grab hold of whoever that whispered, you realise that they are already gone..
 
 You have two options:
 
-A. Demand that the mysterious voice reveals who they are.
-B. You remember that you have a flashlight on your phone.. You turn it on and direct it towards where you 
-heard the voice.
+A. Head to the basement to meet the mysterious informant.
+B. Stay in the drawing room and try to restore order amidst the chaos. Ask the family who it was that whispered? 
 
 What will be your next move, Detective?
 
@@ -307,30 +344,6 @@ What will be your next move, Detective?
     )
 
 def branch_a1_2_1_1():
-    print(
-        """
-Unable to bear the tension any longer, you raise your voice with authority. "Enough of this! Reveal yourself, now!"
-
-As the words leave your mouth, chaos erupts in the room. Some family members scramble in the darkness, 
-attempting to find their way to the door. The atmosphere is charged with fear and confusion.
-
-Amidst the commotion, a faint whisper reaches your ear. "I know who the killer is... Meet me in the basement."
-
-The voice is barely audible, but the urgency in it is unmistakable. You try to discern the speaker, 
-but the darkness conceals them. The message hangs in the air, and you're faced with a crucial decision.
-
-You have two options:
-
-A. Head to the basement to meet the mysterious whisperer. 
-B. Stay in the drawing room and try to restore order amidst the chaos. Perhaps you can find a way to get the lights back on.
-
-
-What will be your next move, Detective?
-
-        """
-    )
-
-def branch_a1_2_1_1_1():
     print(
         """
 With a sense of urgency, you make your way through the darkened mansion, guided only by your flashlight. 
@@ -351,11 +364,87 @@ You realize, too late, the sinister truth hidden within Whodunit Manor.
 The detective's pursuit of the truth ends in a chilling and untimely demise. 
 The secrets of Whodunit Manor, particularly the sinister activities in the basement, 
 remain forever shrouded in mystery.
+
+        """
+    )
+
+def branch_a1_2_1_2():
+    print(
+        """
+You choose to remain in the drawing room, determined to bring some semblance of order to the chaos. 
+Just as you're about to shout, "Who was it that just whispered?" the lights flicker back on, bathing 
+the room in a sudden, startling brightness. Simultaneously, you've already begun to shout, 
+creating a bizarre contrast of noise and light.
+
+The room falls into a stunned silence. The family members exchange concerned looks, 
+clearly taken aback by this unexpected turn of events. You can almost feel their unease radiating from them.
+
+Lady Genevieve clears her throat, breaking the silence. 
+"I think... perhaps we've all had enough excitement for one evening."
+
+There's a murmur of agreement among the family members. It's clear that they're now 
+convinced that you've, for lack of a better term, lost it. They exchange glances, 
+and you catch snippets of conversation about hiring someone else, perhaps that famous detective, Benoit Blanc.
+
+You nod, feeling a strange mixture of embarrassment and bewilderment. 
+As you are make your way to the door, you can't help but wonder how you ended up as the star of this peculiar show.
+
+---
+
+The detective's attempt at restoring order takes an unexpected turn, leading to the family losing confidence in your ability to solve the case. With the decision made to seek the assistance of another detective, you make your exit from Whodunit Manor.
+
+
+        """
+    )
+
+def branch_a1_2_1_1_2():
+    print(
+        """
+
         """
     )
 
 def branch_a1_2_2():
-    print("testing branch_a1_2_2")
+    print(
+    """
+You decide to engage with Uncle Reginald, the stern-looking man who challenges 
+Lady Genevieve's account of the events. With a firm but polite tone, 
+you ask him to share his perspective on that fateful night.
+
+Reginald straightens his posture, his expression determined. 
+"It's important to set the record straight. Mortimer was in high spirits, 
+excited about an upcoming business venture. There was no sign of distress in him."
+
+Before he can continue, a sudden crash resounds through the mansion, 
+followed by the sound of shattering glass. The family members jump in 
+their seats, their faces pale with shock.
+
+You rush to the window and see a shattered vase, its remnants scattered 
+across the floor. Lady Genevieve gasps, clutching her chest.
+
+"What in the world..." she begins, her voice trembling.
+
+Before anyone can react, the lights flicker and then go out entirely. 
+The room is plunged into darkness, and a collective gasp sweeps through the family.
+
+In the chaos that follows, as everybody is trying to get the lights back on
+from the shadows, a voice whispers in your ear. 
+
+"I know what happened that night, meet me in the basement.."
+
+As you try to grab hold of whoever that whispered, you realise that they are already gone..
+
+---
+
+You now have two options:
+
+A. Head to the basement to meet the informant.
+B. Stay in the drawing room and try to restore order amidst the chaos. Ask the family who it was that whispered?
+
+How would you like to proceed, Detective?
+
+    """)
+
 
 def branch_a2():
     print("Testing branch a2")
